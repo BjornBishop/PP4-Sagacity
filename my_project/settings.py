2f5bec14d9fcec7df332d0635e83655a7c3a9c92
Bjorn Bishop <bjornbishop@gmail.com>
2025-01-28 20:37:48 +0000

    changed database in settings and env.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$#lp0wcu649te=gbf$gpetn29s2ekdts5-#qr(_bn!s34pehd_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-bjornbishop-pp4sagacity-j3odq5hp5sj.ws-eu117.gitpod.io']
+ALLOWED_HOSTS = ['8000-bjornbishop-pp4sagacity-j3odq5hp5sj.ws-eu117.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_world'
+    'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +79,15 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("postgresql://neondb_owner:npg_pxZ9MHGsfh7g@ep-odd-lake-a2vch1bn.eu-central-1.aws.neon.tech/chew_spend_armor_816711"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

38f51aedaafaf4c45734f6a7b2b1b16b2c43bfe1
Bjorn Bishop <bjornbishop@gmail.com>
2025-01-30 20:55:57 +0000

    changed directory name to codestar

--- my_project/settings.py
+++ my_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bjornbishop-pp4sagacity-j3odq5hp5sj.ws-eu117.gitpod.io','.herokuapp.com']
 
